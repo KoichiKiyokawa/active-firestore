d7ee5ce7e6da39dca0c3914f0d7e76d12f168378
K.Kiyokawa <koichi20110068@gmail.com>
2020-09-08 21:07:36 +0900

    feat: e.g. new Comment([userId, postId], commentId)

--- src/index.ts
+++ src/index.ts
@@ -1,69 +1,68 @@
-import firebase from "firebase/app";
-
-const splitIds = (ids: string[]): [string[] | null, string | null] => {
-  let parentIds: string[] | null;
-  let thisModelId: string | null;
-  const lastIndex = ids.length - 1;
-  if (ids.length === 0) {
-    parentIds = null;
-    thisModelId = null;
-  } else if (ids.length === 1) {
-    parentIds = null;
-    thisModelId = ids[lastIndex];
-  } else {
-    parentIds = ids.slice(0, lastIndex);
-    thisModelId = ids[lastIndex];
-  }
-
-  return [parentIds, thisModelId];
-};
-
+import { firestore } from "firebase/app";
 export class Base<T extends object> {
-  static collectionName: string = "";
-  static parentModel: typeof Base | null = null;
-  static childModel: typeof Base | null = null;
-  static db: firebase.firestore.Firestore;
+  collectionName: string = "";
+  parent: typeof Base | null = null;
+  childModel: typeof Base | null = null;
+  db!: firestore.Firestore;
 
-  documentReference: firebase.firestore.DocumentReference<T>;
+  collectionReference: firestore.CollectionReference<T>;
+  documentReference: firestore.DocumentReference<T> | null;
 
-  constructor(...ids: string[]) {
-    this.documentReference = Base.db.doc(
-      Base.path(...ids)
-    ) as firebase.firestore.DocumentReference<T>;
+  constructor(parentIdsOrThisId: string[] | string, id?: string) {
+    if (typeof parentIdsOrThisId === "string" && id)
+      throw Error("Invalid initialization!");
+
+    const parentDocumentReference =
+      typeof parentIdsOrThisId === "string"
+        ? this.db
+        : new this.parent!(parentIdsOrThisId, id).documentReference!;
+    this.collectionReference = parentDocumentReference.collection(
+      this.collectionName
+    ) as firestore.CollectionReference<T>;
+
+    if (id) {
+      this.documentReference = this.collectionReference.doc(
+        id
+      ) as firestore.DocumentReference<T>;
+    } else if (typeof parentIdsOrThisId === "string") {
+      // e.g. new Base('id')
+      this.documentReference = this.collectionReference.doc(parentIdsOrThisId);
+    } else {
+      this.documentReference = null;
+    }
   }
 
-  static path(...ids: string[]): string {
-    const [parentIds, thisModelId] = splitIds(ids);
-    const parentPath =
-      parentIds === null || this.parentModel === null
-        ? ""
-        : this.parentModel.path(...parentIds);
-    return `${parentPath}/${this.collectionName}/${thisModelId}`;
+  async add(data: T): Promise<string> {
+    const addedDocumentReference = await this.collectionReference.add(data);
+    return addedDocumentReference.id;
   }
 
   async get(): Promise<T & { id: string }> {
+    if (this.documentReference === null)
+      throw Error(
+        "This model does not have documentReference. Did you mean? getAll"
+      );
+
     const snapshot = await this.documentReference.get();
     return { ...snapshot.data()!, id: snapshot.id };
   }
 
+  async getAll() {
+    const snapshot = await this.collectionReference.get();
+    return snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+  }
+
   async update(data: Partial<T>): Promise<void> {
+    if (this.documentReference === null)
+      throw Error("This model does not have documentReference.");
+
     return this.documentReference.update(data);
   }
 
-  static async find<T extends object>(...ids: string[]) {
-    return new Base<T>(...ids).get()
-  }
+  async delete(): Promise<void> {
+    if (this.documentReference === null)
+      throw Error("This model does not have documentReference.");
 
-  static async findAll<T>(...ids: string[]) {
-    const docRef =
-      this.parentModel === null
-        ? this.db
-        : new this.parentModel(...ids.splice(0, ids.length - 1))
-            .documentReference;
-    const snapshot = await docRef.collection(this.name).get();
-    return snapshot.docs.map((doc) => ({
-      ...doc.data(),
-      id: doc.id,
-    })) as (T & { id: string })[];
+    return this.documentReference.delete();
   }
 }
