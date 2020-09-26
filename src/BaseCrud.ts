52c802e25908525d14b14906444923e5b123dc09
K.Kiyokawa <koichi20110068@gmail.com>
2020-09-27 02:31:07 +0900

    refactor: move mergeIdToSnap to private method

--- src/BaseCrud.ts
+++ src/BaseCrud.ts
@@ -2,15 +2,16 @@ import { firestore } from 'firebase/app'
 import { ERRORS } from './errors'
 
 type WithId<T> = T & { id: string }
-const mergeIdToSnap = <T>(snap: firestore.DocumentSnapshot<T>) => {
-  const data = snap.data()
-  if (!data) return undefined
-  return { ...data, id: snap.id }
-}
 
 export class BaseCrud<T extends Record<string, unknown>> {
   collectionReference?: firestore.CollectionReference<T>
   documentReference?: firestore.DocumentReference<T>
+
+  private mergeIdToSnap(snap: firestore.DocumentSnapshot<T>) {
+    const data = snap.data()
+    if (!data) return undefined
+    return { ...data, id: snap.id }
+  }
 
   async add(data: T): Promise<string> {
     if (this.collectionReference == null) throw ERRORS.NO_COLLECTION_REFERENCE
@@ -22,14 +23,14 @@ export class BaseCrud<T extends Record<string, unknown>> {
   async get(): Promise<WithId<T> | undefined> {
     if (this.documentReference == null) throw ERRORS.NO_DOCUMENT_REFERENCE('Did you mean? getAll')
 
-    return this.documentReference.get().then((snap) => mergeIdToSnap<T>(snap))
+    return this.documentReference.get().then((snap) => this.mergeIdToSnap(snap))
   }
 
   async getAll(): Promise<WithId<T>[]> {
     if (this.collectionReference == null) throw Error()
 
     const snapshot = await this.collectionReference.get()
-    return snapshot.docs.flatMap((doc) => mergeIdToSnap(doc) ?? [])
+    return snapshot.docs.flatMap((doc) => this.mergeIdToSnap(doc) ?? [])
   }
 
   async update(data: Partial<T>): Promise<void> {
