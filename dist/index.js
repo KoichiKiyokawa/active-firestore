dafce0736dbf4f003d161011d35c7aac33dcb35f
K.Kiyokawa <koichi20110068@gmail.com>
2020-09-27 02:16:09 +0900

    chore: delete dist dir

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Base = void 0;
-var Base_1 = require("./Base");
-Object.defineProperty(exports, "Base", { enumerable: true, get: function () { return Base_1.Base; } });
